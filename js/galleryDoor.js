3a0f9e4bf583b71be36b53750d46a3438442d0d2
Lynnc-c <x20130225@gmail.com>
2022-02-25 09:54:16 +0800

    藝廊入口修改

--- js/galleryDoor.js
+++ js/galleryDoor.js
@@ -2,14 +2,14 @@ let xPos = 0;
 
 gsap.timeline()
     .set('.ring', { rotationY:180, cursor:'grab' }) //set initial rotationY so the parallax jump happens off screen
-    .set('.img',  { // apply transform rotations to each image
+    .set('.door',  { // apply transform rotations to each image
         rotateY: (i)=> i*-72,
-        transformOrigin: '50% 50% 600px',
-        z: -600,
+        transformOrigin: '50% 50% 500px',
+        z: -500,
         backgroundPosition:(i)=>getBgPos(i),
         backfaceVisibility:'hidden'
     })    
-    .from('.img', {
+    .from('.door', {
         duration:1.5,
         y:200,
         opacity:0,
@@ -17,12 +17,12 @@ gsap.timeline()
         ease:'expo'
     })
     .add(()=>{
-        $('.img').on('mouseenter', (e)=>{
+        $('.door').on('mouseenter', (e)=>{
             let current = e.currentTarget;
-            gsap.to('.img', {opacity:(i,t)=>(t==current)? 1:0.5, ease:'power3'})
+            gsap.to('.door', {opacity:(i,t)=>(t==current)? 1:0.5, ease:'power3'})
         })
-        $('.img').on('mouseleave', (e)=>{
-            gsap.to('.img', {opacity:1, ease:'power2.inOut'})
+        $('.door').on('mouseleave', (e)=>{
+            gsap.to('.door', {opacity:1, ease:'power2.inOut'})
         })
     }, '-=0.5')
 
@@ -36,13 +36,12 @@ function dragStart(e){
     $(window).on('mousemove touchmove', drag);
 }
 
-
 function drag(e){
     if (e.touches) e.clientX = e.touches[0].clientX;      
 
     gsap.to('.ring', {
         rotationY: '-=' +( (Math.round(e.clientX)-xPos)%360 ),
-        onUpdate:()=>{ gsap.set('.img', { backgroundPosition:(i)=>getBgPos(i) }) }
+        onUpdate:()=>{ gsap.set('.door', { backgroundPosition:(i)=>getBgPos(i) }) }
     });
     xPos = Math.round(e.clientX);
 }
@@ -53,5 +52,5 @@ function dragEnd(e){
 }
 
 function getBgPos(i){ //returns the background-position string to create parallax movement in each image
-    return ( 100-gsap.utils.wrap(0,360,gsap.getProperty('.ring', 'rotationY')-180-i*72)/360*600 )+'px 0px';
+    return ( 100-gsap.utils.wrap(0,360,gsap.getProperty('.ring', 'rotationY')-180-i*72)/360*500 )+'px 0px';
 }
