91a07f6932d287f01e36cb528f28a6b80cc68ffb
ZING15 <zingheartkingandfate@gmail.com>
2022-03-01 03:31:43 +0800

    1.全部新增網頁頁籤icon 2.首頁修改scroll 3.文章首頁新增換頁頁籤 4.header樣式修改

--- js/articlePage.js
+++ js/articlePage.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
+    // 展開留言
     $(".articlePageContent .articlePageWrap .pageContent .shareBar .shareComment .pic:last-child").click(function(){
         $(".articlePageContent .articlePageWrap .commentArea").toggleClass("slideIn");
     });
+    // 展開留言滑到最上面
+    $(".articlePageContent .articlePageWrap .pageContent .shareBar .shareComment .pic:last-child").click(function(){
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+    });
+    // 關閉留言
     $(".articlePageContent .articlePageWrap .commentArea .commentClose").click(function(){
         $(".articlePageContent .articlePageWrap .commentArea").removeClass("slideIn");
     });
