9d9aefa28a05025f1f7b8f99d0691a7ae1db11a5
ZING15 <zingheartkingandfate@gmail.com>
2022-03-14 17:44:08 +0800

    修正

--- php/articlePageCommentSend.php
+++ php/articlePageCommentSend.php
@@ -5,11 +5,19 @@ if($_SERVER['REQUEST_METHOD'] === 'POST') {
     // 確認是否收到js給我的資料
 	$MyComment = json_decode($_POST['MyComment']);
 	$message['message'] = "傳成功了";
-    // 修改資料庫
+    // 抓到留言編號
+    $sqlNumber = "select messageNo from forummes ORDER BY messageNo DESC LIMIT 1";
+    $datas = $pdo->query($sqlNumber);
+    $dataNumber = $datas->fetchAll(PDO::FETCH_ASSOC);
+    $messageNoNew = $dataNumber[0]["messageNo"] + 1;
+    // 抓到時間
+    $today = date('Y-m-d H:i:s');
+    $getData = json_decode($_POST['MyComment']);
 
     // 資料庫語法
     $table ='forummes';//資料庫的table名稱
-    $value =json_decode($_POST['MyComment']);//傳進來的東西，是要可以直接寫進sql的行式
+    $value = "$messageNoNew,$getData,'$today'";//傳進來的東西，是要可以直接寫進sql的行式
+    // echo $value;
     $sql = "insert into $table value ($value)";
     // 傳到資料庫裏面去
     try {
