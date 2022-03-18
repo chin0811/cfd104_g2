fa9cb69e6aab5557c7d1e285d85a15e3d286fc8d
chin0811 <chin.frontend@gmail.com>
2022-03-18 11:10:03 +0800

    訂單後台

---
+++ php/backOrderSelect.php
@@ -0,0 +1,17 @@
+<?php 
+header('Content-Type: application/json; charset=utf-8');
+//在這裡取得資料的門票
+try {
+	require_once("g2_dataConnect.php");
+	//執行sql指令並取得pdoStatement
+	$sql = "select * from actord join com on actord.productNumber = com.prodNo where com.saleStatus = '已販售'";
+	$prodImage = $pdo->query($sql);
+	$prodRow = $prodImage->fetchAll(PDO::FETCH_ASSOC);
+	echo json_encode($prodRow,JSON_UNESCAPED_UNICODE);
+
+} catch (Exception $e) {
+	echo "錯誤行號 : ", $e->getLine(), "<br>";
+	echo "錯誤原因 : ", $e->getMessage(), "<br>";
+	//echo "系統暫時不能正常運行，請稍後再試<br>";	
+}
+?>
